ccpnmr/AnalysisScreen

Use git commit hashes as revision(s) in mainWindow title.

__main__.py
@@ -2,8 +2,11 @@ __author__ = 'TJ'
 
 from ccpn.framework import Framework
 from ccpn.AnalysisScreen.AnalysisScreen import Screen as Application
+from ccpn.framework.Version import applicationVersion
 
 if __name__ == '__main__':
+  from ccpn.util.GitTools import getAllRepositoriesGitCommit
+  applicationVersion = 'development: {AnalysisScreen:.8s}'.format(**getAllRepositoriesGitCommit())
 
   # argument parser
   parser = Framework.defineProgramArguments()
@@ -11,7 +14,7 @@ if __name__ == '__main__':
   # add any additional commandline argument here
   commandLineArguments = parser.parse_args()
 
-  application = Application('AnalysisScreen', 'ALPHA-1', commandLineArguments)
+  application = Application('AnalysisScreen', applicationVersion, commandLineArguments)
   Framework._getApplication = lambda: application
   application.start()
 

ccpnmr/AnalysisAssign

__main__.py
@@ -5,6 +5,8 @@ from ccpn.AnalysisAssign.AnalysisAssign import Assign as Application
 from ccpn.framework.Version import applicationVersion
 
 if __name__ == '__main__':
+  from ccpn.util.GitTools import getAllRepositoriesGitCommit
+  applicationVersion = 'development: {AnalysisAssign:.8s}'.format(**getAllRepositoriesGitCommit())
 
   # argument parser
   parser = Framework.defineProgramArguments()