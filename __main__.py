0ba631997fdd33358e9d80a66837094189ffbc64
Ed Brooksbank <ejb66@le.ac.uk>
2017-06-07 11:48:29 +0100

    added compareNef to /bin

--- __main__.py
+++ __main__.py
@@ -1,21 +1,3 @@
-
-from ccpn.framework import Framework
-from ccpn.AnalysisAssign.AnalysisAssign import Assign as Application
-from ccpn.framework.Version import applicationVersion
-
-if __name__ == '__main__':
-  from ccpn.util.GitTools import getAllRepositoriesGitCommit
-  applicationVersion = 'development: {AnalysisAssign:.8s}'.format(**getAllRepositoriesGitCommit())
-
-  # argument parser
-  parser = Framework.defineProgramArguments()
-
-  # add any additional commandline argument here
-  commandLineArguments = parser.parse_args()
-
-  application = Application('AnalysisAssign', applicationVersion, commandLineArguments)
-  Framework._getApplication = lambda: application
-  application.start()
 #=========================================================================================
 # Licence, Reference and Credits
 #=========================================================================================
@@ -40,3 +22,21 @@ __date__ = "$Date: 2017-04-07 10:28:40 +0000 (Fri, April 07, 2017) $"
 #=========================================================================================
 # Start of code
 #=========================================================================================
+
+from ccpn.framework import Framework
+from ccpn.AnalysisAssign.AnalysisAssign import Assign as Application
+from ccpn.framework.Version import applicationVersion
+
+if __name__ == '__main__':
+  from ccpn.util.GitTools import getAllRepositoriesGitCommit
+  applicationVersion = 'development: {AnalysisAssign:.8s}'.format(**getAllRepositoriesGitCommit())
+
+  # argument parser
+  parser = Framework.defineProgramArguments()
+
+  # add any additional commandline argument here
+  commandLineArguments = parser.parse_args()
+
+  application = Application('AnalysisAssign', applicationVersion, commandLineArguments)
+  Framework._getApplication = lambda: application
+  application.start()
