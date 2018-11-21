2df4bdf2b1f90d9bd07c2b1fcbf8d59a9761ccf4
Geerten Vuister <gv29@leicester.ac.uk>
2018-11-21 19:48:44 +0000

    Cleaned-up Notifiers, annotations, refactored 'rename' on NmrAtom/ChemicalShift

--- modules/BackboneAssignmentModule.py
+++ modules/BackboneAssignmentModule.py
@@ -733,28 +733,28 @@ def markNmrAtoms(mainWindow, nmrAtoms:typing.List[NmrAtom]):
 
 
 
-#=====  Just some code to 'save' =====
-def hasNmrResidue(nmrChain, residueCode):
-  "Simple function to check if sequenCode is found within the nmrResidues of nmrChain"
-  resCodes = [res.sequenceCode for res in nmrChain.nmrResidues]
-  return (residueCode in resCodes)
-
-
-def endOfchain(nmrResidue):
-  # changes to end of connected chain; not a good idea
-  if nmrResidue.nmrChain.isConnected:
-    if nmrResidue.sequenceCode.endswith('-1'):
-      nmrResidue = nmrResidue.nmrChain.mainNmrResidues[0].getOffsetNmrResidue(-1)
-    else:
-      nmrResidue = nmrResidue.nmrChain.mainNmrResidues[-1]
-  return nmrResidue
-
-
-def getPids(fromObject, attributeName):
-  "Get a list of pids fromObject.attributeName or None on error"
-  if not hasattr(fromObject, attributeName): return None
-  return [obj.pid for obj in getattr(fromObject, attributeName)]
-#===== end code save =====
+# #=====  Just some code to 'save' =====
+# def hasNmrResidue(nmrChain, residueCode):
+#   "Simple function to check if sequenCode is found within the nmrResidues of nmrChain"
+#   resCodes = [res.sequenceCode for res in nmrChain.nmrResidues]
+#   return (residueCode in resCodes)
+#
+#
+# def endOfchain(nmrResidue):
+#   # changes to end of connected chain; not a good idea
+#   if nmrResidue.nmrChain.isConnected:
+#     if nmrResidue.sequenceCode.endswith('-1'):
+#       nmrResidue = nmrResidue.nmrChain.mainNmrResidues[0].getOffsetNmrResidue(-1)
+#     else:
+#       nmrResidue = nmrResidue.nmrChain.mainNmrResidues[-1]
+#   return nmrResidue
+#
+#
+# def getPids(fromObject, attributeName):
+#   "Get a list of pids fromObject.attributeName or None on error"
+#   if not hasattr(fromObject, attributeName): return None
+#   return [obj.pid for obj in getattr(fromObject, attributeName)]
+# #===== end code save =====
 
 
 if __name__ == '__main__':
