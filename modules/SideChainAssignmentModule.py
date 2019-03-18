5c40418a516d3eb10be79b85527572a4dbd474d0
Ed Brooksbank <ejb66@le.ac.uk>
2019-03-18 15:43:31 +0000

    fixed missing classname for SideChainAssignmentModule

--- modules/SideChainAssignmentModule.py
+++ modules/SideChainAssignmentModule.py
@@ -1,16 +1,44 @@
-from ccpn.core.lib.Notifiers import Notifier
+"""Module Documentation here
+"""
+#=========================================================================================
+# Licence, Reference and Credits
+#=========================================================================================
+__copyright__ = "Copyright (C) CCPN project (http://www.ccpn.ac.uk) 2014 - 2019"
+__credits__ = ("Ed Brooksbank, Luca Mureddu, Timothy J Ragan & Geerten W Vuister")
+__licence__ = ("CCPN licence. See http://www.ccpn.ac.uk/v3-software/downloads/license")
+__reference__ = ("Skinner, S.P., Fogh, R.H., Boucher, W., Ragan, T.J., Mureddu, L.G., & Vuister, G.W.",
+                 "CcpNmr AnalysisAssign: a flexible platform for integrated NMR analysis",
+                 "J.Biomol.Nmr (2016), 66, 111-124, http://doi.org/10.1007/s10858-016-0060-y")
+#=========================================================================================
+# Last code modification
+#=========================================================================================
+__modifiedBy__ = "$modifiedBy: CCPN $"
+__dateModified__ = "$dateModified: 2017-07-07 16:32:21 +0100 (Fri, July 07, 2017) $"
+__version__ = "$Revision: 3.0.b5 $"
+#=========================================================================================
+# Created
+#=========================================================================================
+__author__ = "$Author: Geerten Vuister $"
+__date__ = "$Date: 2017-04-07 10:28:40 +0000 (Fri, April 07, 2017) $"
+#=========================================================================================
+# Start of code
+#=========================================================================================
+
 from ccpn.util import Common
 from ccpn.AnalysisAssign.modules.PickAndAssignModule import PickAndAssignModule
 from ccpn.ui.gui.lib.SpectrumDisplay import makeStripPlot, makeStripPlotFromSingles
+from ccpn.core.lib.Notifiers import Notifier
 
 from ccpn.ui.gui.lib.Strip import matchAxesAndNmrAtoms
 
 
 class SideChainAssignmentModule(PickAndAssignModule):
 
-    def __init__(self, mainWindow, name='Pick and Assign'):
+    className = 'SideChainAssignmentModule'
 
-        PickAndAssignModule.__init__(self, mainWindow=mainWindow, name=name)
+    def __init__(self, mainWindow, name='Sidechain Assignment'):
+
+        super().__init__(mainWindow=mainWindow, name=name)
 
         self.mainWindow = mainWindow
         self.application = mainWindow.application
